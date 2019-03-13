0bfa32a4e1baf768b887197cf813a45680962f59
Olivier <ishimweolivier1@gmail.com>
2019-03-14 02:46:09 +0800

    Changes

---
+++ finalProject/src/main/java/mum/ea/controller/LoginController.java
@@ -0,0 +1,10 @@
+package mum.ea.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class LoginController {
+
+}
